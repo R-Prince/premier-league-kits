a1e7defe2838787ecfff0d5a1a173e2f5c6ef382
R-Prince <reneprince93@icloud.com>
2020-07-12 13:04:35 +0000

    Added JS to index.html

---
+++ script.js
@@ -0,0 +1,38 @@
+function arsenal(){
+    document.getElementById("club-logo").src = "https://icons.iconarchive.com/icons/giannis-zographos/english-football-club/96/Arsenal-FC-icon.png";
+
+    document.getElementById("white").id = "whiteArsenal";
+
+    document.getElementById("main-body").id = "main-bodyArsenal";
+
+    document.getElementById("team-kit").src = "https://resources.premierleague.com/photos/2019/07/04/29e93027-32b2-48c7-a3e5-62b470b55784/ARS_HK_516.png?width=260&height=330";
+
+    document.getElementById("team-header").innerText="Arsenal F.C";
+    
+};
+
+function villa(){
+    document.getElementById("club-logo").src = "https://icons.iconarchive.com/icons/giannis-zographos/english-football-club/96/Aston-Villa-icon.png";
+
+    document.getElementById("team-kit").src = "https://resources.premierleague.com/photos/2019/07/09/69a3c969-9e2f-4237-82f8-aa532134f805/AST_HK_516.png?width=260&height=330";
+
+    document.getElementById("team-header").innerText="Aston Villa F.C";
+
+    document.getElementById("whiteArsenal").id = "whiteVilla";
+
+    document.getElementById("main-bodyArsenal").id = "main-bodyVilla";
+
+};
+
+function chelsea(){
+    document.getElementById("team-header").innerText="Chelsea F.C";
+
+    document.getElementById("whiteVilla").id = "white";
+
+    document.getElementById("main-bodyVilla").id = "main-body";
+
+    document.getElementById("club-logo").src = "https://icons.iconarchive.com/icons/giannis-zographos/english-football-club/96/Chelsea-FC-icon.png";
+
+    document.getElementById("team-kit").src = "https://resources.premierleague.com/photos/2020/07/01/84761cbe-ffd4-4e56-a04e-b9a8edbbc4ad/CHE_2021_HK_S1.png?width=260&height=330";
+
+}
